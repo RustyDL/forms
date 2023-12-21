a8a8dbf07f76be7db2de7dc553cf9e8cb9e275cd
Jason Garcia <146444354+RustyDL@users.noreply.github.com>
2023-12-20 21:30:06 -0500

    update info

--- react forms/src/App.jsx
+++ react forms/src/App.jsx
@@ -1,14 +1,17 @@
-import Authenticate from "./authenticate";
-import SignUpForm from "./signupform";
-export default function App() {
-  const [count, setCount] = useState(0)
+import { useState } from 'react';
+import SignUpForm from './components/signup/SignUpForm';
+import Authenticate from './components/auth/Authenticate';
+import './App.css';
+
+function App() {
+  const [authToken, setAuthToken] = useState(null);
 
   return (
     <>
-    <Authenticate />
-    <SignUpForm />
-  </>
-);
+    <SignUpForm token={token} setToken={setToken} />
+    <Authenticate token={token} setToken={setToken} />
+    </>
+  )
 }
 
 export default App
